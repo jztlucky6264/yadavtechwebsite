cc313a5a2d6f92b5530575c670fc74bda9ff7bde
luckyyadav <luckyyadav6264@gmail.com>
2021-04-17 19:32:46 -1200

    second commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <>
       <footer className="w-100 bg-light text-center my-5 footer_sec ">
-        <p>©️ 2020 YadavTech. All Rights Reserved | Terms and Conditions</p>
+        <p>©️ {year} YadavTech. All Rights Reserved | Terms and Conditions</p>
       </footer>
     </>
   );
