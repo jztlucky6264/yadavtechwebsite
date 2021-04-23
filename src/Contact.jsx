b77448fe94eb3c9b04e51db1290fd0969b6f70e1
luckyyadav <luckyyadav6264@gmail.com>
2021-04-23 06:22:37 -1200

    last third commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -32,53 +32,53 @@ const Contact = () => {
           <div className="col-md-6 col-1o mx-auto">
             <form
               onSubmit={formSubmit}
-              className="row g-3 needs-validation"
-              noValidate
+              class="row g-3 needs-validation"
+              novalidate
             >
-              <div className="col-md-4">
-                <label htmlFor="validationCustom01" className="form-label">
+              <div class="col-md-4">
+                <label for="validationCustom01" class="form-label">
                   FullName
                 </label>
                 <input
                   type="text"
-                  className="form-control"
+                  class="form-control"
+                  id="validationCustom01"
+                  placeholder="Enter your name"
                   name="fullname"
                   value={data.fullname}
                   onChange={InputEvent}
-                  id="validationCustom01"
                   required
                 />
-                <div className="valid-feedback">Looks good!</div>
+                <div class="valid-feedback">Looks good!</div>
               </div>
-              <div className="col-md-4">
-                <label htmlFor="validationCustom02" className="form-label">
+              <div class="col-md-4">
+                <label for="validationCustom02" class="form-label">
                   Phone
                 </label>
                 <input
-                  type="text"
+                  type="Text"
+                  class="form-control"
+                  placeholder="Mobile Number"
                   name="phone"
                   value={data.phone}
                   onChange={InputEvent}
-                  className="form-control"
                   id="validationCustom02"
                   required
                 />
-                <div className="valid-feedback">Looks good!</div>
+                <div class="valid-feedback">Looks good!</div>
               </div>
-              <div className="col-md-4">
-                <label
-                  htmlFor="validationCustomUsername"
-                  className="form-label"
-                >
+              <div class="col-md-4">
+                <label for="validationCustomUsername" class="form-label">
                   Username
                 </label>
-                <div className="input-group has-validation">
-                  <span className="input-group-text" id="inputGroupPrepend">
+                <div class="input-group has-validation">
+                  <span class="input-group-text" id="inputGroupPrepend">
                     @
                   </span>
                   <input
                     type="text"
-                    className="form-control"
+                    class="form-control"
+                    placeholder="name@example.com"
                     name="email"
                     value={data.email}
                     onChange={InputEvent}
@@ -86,47 +86,67 @@ const Contact = () => {
                     aria-describedby="inputGroupPrepend"
                     required
                   />
-                  <div className="invalid-feedback">
-                    Please choose a username.
-                  </div>
+                  <div class="invalid-feedback">Please choose a username.</div>
                 </div>
               </div>
-              <div className="col-md-6">
-                <label htmlFor="validationCustom03" className="form-label">
+              <div class="col-md-6">
+                <label for="validationCustom03" class="form-label">
                   City
                 </label>
                 <input
                   type="text"
-                  className="form-control"
+                  class="form-control"
+                  name="city"
+                  value={data.city}
+                  onChange={InputEvent}
                   id="validationCustom03"
                   required
                 />
-                <div className="invalid-feedback">
-                  Please provide a valid city.
-                </div>
+                <div class="invalid-feedback">Please provide a valid city.</div>
               </div>
-
-              <div className="col-12">
-                <div className="form-check">
+              <div class="col-md-3">
+                <label for="validationCustom04" class="form-label">
+                  State
+                </label>
+                <select class="form-select" id="validationCustom04" required>
+                  <option selected disabled value="">
+                    Choose...
+                  </option>
+                  <option>Delhi</option>
+                </select>
+                <div class="invalid-feedback">Please select a valid state.</div>
+              </div>
+              <div class="col-md-3">
+                <label for="validationCustom05" class="form-label">
+                  Zip
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="validationCustom05"
+                  required
+                />
+                <div class="invalid-feedback">Please provide a valid zip.</div>
+              </div>
+              <div class="col-12">
+                <div class="form-check">
                   <input
-                    className="form-check-input"
+                    class="form-check-input"
                     type="checkbox"
-                    name="city"
-                    value={data.city}
-                    onChange={InputEvent}
+                    value=""
                     id="invalidCheck"
                     required
                   />
-                  <label className="form-check-label" htmlFor="invalidCheck">
+                  <label class="form-check-label" for="invalidCheck">
                     Agree to terms and conditions
                   </label>
-                  <div className="invalid-feedback">
+                  <div class="invalid-feedback">
                     You must agree before submitting.
                   </div>
                 </div>
               </div>
-              <div className="col-12">
-                <button className="btn btn-primary" type="submit">
+              <div class="col-12">
+                <button class="btn btn-primary" type="submit">
                   Submit form
                 </button>
               </div>
